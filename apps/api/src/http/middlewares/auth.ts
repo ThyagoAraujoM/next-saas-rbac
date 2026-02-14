50ec55f34048df3d14c9f0a7d165868a7a9170e7
Thyago Motta <thyagoaraujomotta@gmail.com>
2026-02-14 13:19:06 -0300

    feat: Criando middleware de autenticação

---
+++ apps/api/src/http/middlewares/auth.ts
@@ -0,0 +1,24 @@
+import type { FastifyInstance } from 'fastify'
+import { UnauthorizedError } from '../routes/_errors/unauthorized-error'
+import fastifyPlugin from 'fastify-plugin'
+
+export const auth = fastifyPlugin(async (app: FastifyInstance) => {
+  app.addHook('preHandler', async (request, reply) => {
+    request.getCurrentUserId = async () => {
+      try {
+        const { sub } = await request.jwtVerify<{ sub: string }>()
+
+        return sub
+      } catch (error) {
+        throw new UnauthorizedError('Invalid authentication token')
+      }
+    }
+    try {
+      const { sub } = await request.jwtVerify<{ sub: string }>()
+
+      return sub
+    } catch (error) {
+      throw new UnauthorizedError('Invalid authentication token')
+    }
+  })
+})
