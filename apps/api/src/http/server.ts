7ab69da4bbdca28ad4739b8df93000ab69bf8a61
Thyago Motta <thyagoaraujomotta@gmail.com>
2026-02-13 21:15:53 -0300

    feat: Setup do Swagger

--- apps/api/src/http/server.ts
+++ apps/api/src/http/server.ts
@@ -1,20 +1,39 @@
 import fastifyCors from '@fastify/cors'
 import { fastify } from 'fastify'
+import fastifySwagger from '@fastify/swagger'
+
 import {
-  // jsonSchemaTransform,
+  jsonSchemaTransform,
   serializerCompiler,
   validatorCompiler,
   ZodTypeProvider,
 } from 'fastify-type-provider-zod'
 import { createAccount } from './routes/auth/create-account'
+import fastifySwaggerUi from '@fastify/swagger-ui'
 
 const app = fastify().withTypeProvider<ZodTypeProvider>()
 
 app.setSerializerCompiler(serializerCompiler)
 app.setValidatorCompiler(validatorCompiler)
 
-app.register(fastifyCors)
+app.register(fastifySwagger, {
+  openapi: {
+    info: {
+      title: 'Next.js SaaS',
+      description:
+        'Full-stack SaaS boilerplate built with Next.js, Multi-tenant, RBAC, Fastify, Prisma, and more.',
+      version: '1.0.0',
+    },
+    servers: [],
+  },
+  transform: jsonSchemaTransform,
+})
 
+app.register(fastifySwaggerUi, {
+  routePrefix: '/docs',
+})
+
+app.register(fastifyCors)
 app.register(createAccount)
 
 app.listen({ port: 3000 }).then(() => {
