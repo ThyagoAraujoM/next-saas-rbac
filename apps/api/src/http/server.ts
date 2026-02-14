55e42db15e15e792d54a5bc3c14b09be9cc5f597
Thyago Motta <thyagoaraujomotta@gmail.com>
2026-02-13 22:27:57 -0300

    feat: Rota: Autenticação via senha

--- apps/api/src/http/server.ts
+++ apps/api/src/http/server.ts
@@ -1,15 +1,17 @@
 import fastifyCors from '@fastify/cors'
 import { fastify } from 'fastify'
 import fastifySwagger from '@fastify/swagger'
-
+import fastifyJwt from '@fastify/jwt'
+import fastifySwaggerUi from '@fastify/swagger-ui'
 import {
   jsonSchemaTransform,
   serializerCompiler,
   validatorCompiler,
   ZodTypeProvider,
 } from 'fastify-type-provider-zod'
+
 import { createAccount } from './routes/auth/create-account'
-import fastifySwaggerUi from '@fastify/swagger-ui'
+import { authenticateWithPassword } from './routes/auth/authenticate-with-password'
 
 const app = fastify().withTypeProvider<ZodTypeProvider>()
 
@@ -33,8 +35,13 @@ app.register(fastifySwaggerUi, {
   routePrefix: '/docs',
 })
 
+app.register(fastifyJwt, {
+  secret: 'my-jwt-secret',
+})
+
 app.register(fastifyCors)
 app.register(createAccount)
+app.register(authenticateWithPassword)
 
 app.listen({ port: 3000 }).then(() => {
   console.log('HTTP server running!')
