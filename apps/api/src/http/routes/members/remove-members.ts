33c40ede04f6d75dbc464cadaf44fb1dcb6ccf70
Thyago Motta <thyagoaraujomotta@gmail.com>
2026-02-15 14:55:56 -0300

    feat: Rota: Remover membro

--- apps/api/src/http/routes/members/remove-members.ts
+++ apps/api/src/http/routes/members/remove-members.ts
@@ -5,27 +5,23 @@ import type { ZodTypeProvider } from 'fastify-type-provider-zod'
 import { z } from 'zod'
 
 import { getUserPermission } from '@/utils/get-user-permisseions'
-import { roleSchema } from '@saas/auth'
 import { UnauthorizedError } from '../_errors/unauthorized-error'
 import { BadRequestError } from '../_errors/bad-request-error'
 
-export async function updateMembers(app: FastifyInstance) {
+export async function removeMembers(app: FastifyInstance) {
   app
     .withTypeProvider<ZodTypeProvider>()
     .register(auth)
-    .put(
+    .delete(
       '/organization/:slug/members/:memberId',
       {
         schema: {
           tags: ['Members'],
-          summary: 'Update a member of an organization',
+          summary: 'Remove a member of an organization',
           security: [{ bearerAuth: [] }],
           params: z.object({
             slug: z.string(),
             memberId: z.uuid(),
-          }),
-          body: z.object({
-            role: roleSchema,
           }),
           response: {
             204: z.void(),
@@ -39,7 +35,7 @@ export async function updateMembers(app: FastifyInstance) {
 
         const { cannot } = getUserPermission(userId, membership.role)
         if (cannot('update', 'User')) {
-          throw new UnauthorizedError(`You're not allowed to update this member`)
+          throw new UnauthorizedError(`You're not allowed to delete this member from this organization`)
         }
 
         const member = await prisma.member.findUnique({
@@ -55,14 +51,9 @@ export async function updateMembers(app: FastifyInstance) {
           throw new BadRequestError(`Member not found`)
         }
 
-        const { role } = request.body
-
-        await prisma.member.update({
+        await prisma.member.delete({
           where: {
             id: member.id,
-          },
-          data: {
-            role: role,
           },
         })
 
--- apps/api/src/http/server.ts
+++ apps/api/src/http/server.ts
@@ -27,6 +27,7 @@ import { getProjects } from './routes/projects/get-projects'
 import { updateProject } from './routes/projects/update-project'
 import { getMembers } from './routes/members/get-members'
 import { updateMembers } from './routes/members/update-members'
+import { removeMembers } from './routes/members/remove-members'
 
 const app = fastify().withTypeProvider<ZodTypeProvider>()
 
@@ -88,6 +89,7 @@ app.register(updateProject)
 
 app.register(getMembers)
 app.register(updateMembers)
+app.register(removeMembers)
 
 app.listen({ port: env.SERVER_PORT }).then(() => {
   console.log('HTTP server running!')
