837180550e1ac468a591549c77acb58dfad1107d
Thyago Motta <thyagoaraujomotta@gmail.com>
2026-02-15 12:05:34 -0300

    feat: Rota: Atualizar projeto

---
+++ apps/api/src/http/routes/projects/update-project.ts
@@ -0,0 +1,72 @@
+import { auth } from '@/http/middlewares/auth'
+import { prisma } from '@/lib/prisma'
+import type { FastifyInstance } from 'fastify'
+import type { ZodTypeProvider } from 'fastify-type-provider-zod'
+import z from 'zod'
+import { BadRequestError } from '../_errors/bad-request-error'
+import { getUserPermission } from '@/utils/get-user-permisseions'
+import { projectSchema } from '@saas/auth'
+
+export async function updateProject(app: FastifyInstance) {
+  app
+    .withTypeProvider<ZodTypeProvider>()
+    .register(auth)
+    .put(
+      '/organization/:slug/projects/:projectId',
+      {
+        schema: {
+          tags: ['Projects'],
+          summary: 'Update a project',
+          security: [{ bearerAuth: [] }],
+          params: z.object({
+            slug: z.string(),
+            projectId: z.string().uuid(),
+          }),
+          body: z.object({
+            name: z.string(),
+            description: z.string(),
+          }),
+          response: {
+            204: z.void(),
+          },
+        },
+      },
+      async (request, reply) => {
+        const { slug, projectId } = request.params
+        const userId = await request.getCurrentUserId()
+        const { membership, organization } = await request.getUserMembership(slug)
+
+        const project = await prisma.project.findUnique({
+          where: {
+            id: projectId,
+            organizationId: organization.id,
+          },
+        })
+
+        if (!project) {
+          throw new BadRequestError('Project not found')
+        }
+
+        const { cannot } = getUserPermission(userId, membership.role)
+        const authProject = projectSchema.parse(project)
+
+        if (cannot('update', authProject)) {
+          throw new BadRequestError(`You're not allowed to update this project`)
+        }
+
+        const { name, description } = request.body
+
+        await prisma.project.update({
+          where: {
+            id: projectId,
+          },
+          data: {
+            name,
+            description,
+          },
+        })
+
+        return reply.status(204).send()
+      }
+    )
+}
