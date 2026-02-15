14d06fdac7ce0ac466e9565727b591ef4e22276c
Thyago Motta <thyagoaraujomotta@gmail.com>
2026-02-15 10:21:10 -0300

    feat: Rota: Criação de projetos

---
+++ apps/api/src/http/routes/projects/create-project.ts
@@ -0,0 +1,63 @@
+import { auth } from '@/http/middlewares/auth'
+import { prisma } from '@/lib/prisma'
+import type { FastifyInstance } from 'fastify'
+import type { ZodTypeProvider } from 'fastify-type-provider-zod'
+import z from 'zod'
+import { BadRequestError } from '../_errors/bad-request-error'
+import { createSlug } from '@/utils/create-slug'
+import { getUserPermission } from '@/utils/get-user-permisseions'
+
+export async function createProject(app: FastifyInstance) {
+  app
+    .withTypeProvider<ZodTypeProvider>()
+    .register(auth)
+    .post(
+      '/organization/:slug/projects',
+      {
+        schema: {
+          tags: ['Projects'],
+          summary: 'Create a new project within an organization',
+          security: [{ bearerAuth: [] }],
+          body: z.object({
+            name: z.string(),
+            description: z.string(),
+          }),
+          params: z.object({
+            slug: z.string(),
+          }),
+          response: {
+            201: z.object({
+              projectId: z.string(),
+            }),
+          },
+        },
+      },
+      async (request, reply) => {
+        const { slug } = request.params
+        const userId = await request.getCurrentUserId()
+        const { membership, organization } = await request.getUserMembership(slug)
+
+        const { cannot } = getUserPermission(userId, membership.role)
+
+        if (cannot('create', 'Project')) {
+          throw new BadRequestError('You do not have permission to create a project in this organization')
+        }
+
+        const { name, description } = request.body
+
+        const project = await prisma.project.create({
+          data: {
+            name,
+            slug: createSlug(name),
+            description,
+            organizationId: organization.id,
+            ownerId: userId,
+          },
+        })
+
+        return reply.status(201).send({
+          projectId: project.id,
+        })
+      }
+    )
+}
