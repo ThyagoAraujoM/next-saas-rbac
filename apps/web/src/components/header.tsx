ae94ef3a70b7759c9328c07311ecb0f13b816eda
Thyago Motta <thyagoaraujomotta@gmail.com>
2026-02-19 13:16:31 -0300

    feat: Componente: organization-switcher

--- apps/web/src/components/header.tsx
+++ apps/web/src/components/header.tsx
@@ -1,12 +1,17 @@
 import Image from 'next/image'
 import Logo from '../assets/logo.svg'
 import { ProfileButton } from './profile-button'
+import { OrganizationSwitcher } from './organization-switcher'
+import { Slash } from 'lucide-react'
 
 export default async function Header() {
   return (
     <div className="mx-auto flex max-w-300 items-center justify-between">
       <div className="flex items-center gap-3">
         <Image src={Logo} alt="Logo" className="size-6" />
+        <Slash className="text-border size-3 -rotate-24" />
+
+        <OrganizationSwitcher />
       </div>
 
       <div className="flex items-center gap-4">
