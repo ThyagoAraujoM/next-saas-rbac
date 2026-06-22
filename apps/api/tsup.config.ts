d0034f41153c73935dbef5dea357f06cc50c8bfa
Thyago Motta <thyagoaraujomotta@gmail.com>
2026-06-21 23:02:20 -0300

    chore: add build and start script to api

---
+++ apps/api/tsup.config.ts
@@ -0,0 +1,12 @@
+import { defineConfig } from 'tsup'
+
+export default defineConfig({
+  format: ['esm'],
+  entry: ['src'],
+  platform: 'node',
+  splitting: false,
+  sourcemap: true,
+  clean: true,
+  noExternal: ['@saas/auth', '@saas/env'],
+  external: ['dotenv'],
+})
